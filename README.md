10a01d2dc836eced54d3041ad21d1dda17d8ce89
lofyer <lofyer@gmail.com>
2019-04-01 20:16:55 +0800

    update

--- README.md
+++ README.md
@@ -4,14 +4,18 @@ Aliyun ZStack Topology Generator
 ## Installation
 1. Install graphviz.
 
-https://www.graphviz.org/download/
+    https://www.graphviz.org/download/
 
 2. Install essential python libraries.
     
-    pip2/pip3 install pymysql
+```bash
+pip2/pip3 install pymysql
+```
 
 ## Using
 
 Run topo-generator.py:
 
-    ./topo-generator.py
+```python
+./topo-generator.py
+```
--- topo-generator.py
+++ topo-generator.py
@@ -2,21 +2,29 @@
 # -*- coding: UTF-8 -*-
 import pymysql
 
-db = pymysql.connect("172.20.1.11",
+db = pymysql.connect("172.20.0.10",
     "root",
     "zstack.mysql.password",
     "zstack" )
 cursor = db.cursor()
 cursor.execute("SELECT VERSION()")
-print(cursor.fetchone())
+print("Mysql Version: ", cursor.fetchone())
 
 # Management node
-cursor.execute("select hostName from ManagementNodeVO")
-print(list(cursor.fetchall())[0])
+cursor.execute("select uuid,hostName from ManagementNodeVO")
+print("Management Nodes: ", list(cursor.fetchall()))
+
+# Zone
+cursor.execute("select uuid,name,type from ZoneEO")
+print("Zones: ", list(cursor.fetchall()))
+
+# Cluster
+cursor.execute("select uuid,zoneUuid,name from ClusterEO")
+print("Compute Nodes: ", list(cursor.fetchall()))
 
 # Compute node
-cursor.execute("select managementIp from HostEO")
-print(list(cursor.fetchall())[0])
+cursor.execute("select uuid,managementIp from HostEO")
+print("Compute Nodes: ", list(cursor.fetchall()))
 
 # Storage type
 
